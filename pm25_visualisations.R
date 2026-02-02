dca9c5366a0f9b1a0c71c4f2ffcb8215b2b06ea6
data-science-coursework <katwhitingggg@gmail.com>
2026-02-02 18:15:06 +0000

    Update pm25_visualisations.R

--- pm25_visualisations.R
+++ pm25_visualisations.R
@@ -1,1 +1,15 @@
 
+# PM2.5 visualistions
+# IJC445 coursework
+
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+  
+  # set a colour palette to be used for all visualisations
+  site_cols <- c(
+  "Leeds Centre" = "#1f3c88",
+  "Leeds Headingley Kerbside" = "#6aaed6",
+  "Sheffield Barnsley Road" = "darkgoldenrod2",
+  "Sheffield Tinsley" = "#d73027")
+
