002c6f7a2e621494449dd6e117d768e2c827e46a
data-science-coursework <katwhitingggg@gmail.com>
2026-02-03 11:36:35 +0000

    Update pm25_visualisations.R

--- pm25_visualisations.R
+++ pm25_visualisations.R
@@ -24,9 +24,7 @@ pm25_daily %>%
     title = "Distribution of daily PM2.5 concentrations by year",
     subtitle = "Sheffield Barnsley Road (2017–2025)",
     x = "Year",
-    y = expression("PM2.5 ("*mu*"g/m"^3*")"))
-    +
-    theme_minimal()
+    y = expression("PM2.5 ("*mu*"g/m"^3*")"))  + theme_minimal()
 
 # Create the seasonal bar chart including all four monitoring sites
 pm25_seasonal_overall %>%
@@ -38,20 +36,9 @@ pm25_seasonal_overall %>%
   scale_fill_manual(values = site_cols, name = "Monitoring site") +
   labs( title = "Seasonal mean PM2.5 concentrations",
     x = "Season",
-    y = expression("PM2.5 ("*mu*"g/m"^3*")"))
-   +
-   theme_minimal()
+    y = expression("PM2.5 ("*mu*"g/m"^3*")"))  + theme_minimal()
 
 # Create the diurnal line plot including all four monitoring locations
-firstly, change measurements from 24:00 hours to 0 hours to match convention for reporting of midnight
-pm25_diurnal <- pm25_hourly %>% 
-  mutate(plot_hour = ifelse(hour == 24, 0, hour)) %>%
-  group_by(site_name, plot_hour) %>%
-  summarise(
-    mean_pm25 = mean(pm25, na.rm = TRUE),
-    .groups = "drop")
-
-#then create plot3
 
 ggplot(pm25_diurnal,
  aes(x = plot_hour, y = mean_pm25, colour = site_name)) +
@@ -61,8 +48,7 @@ ggplot(pm25_diurnal,
   labs(
     title = "Diurnal variation in PM2.5 concentrations",
     x = "Hour of day",
-    y = expression("PM2.5 ("*mu*"g/m"^3*")")
-  ) + theme_minimal()
+    y = expression("PM2.5 ("*mu*"g/m"^3*")")) + theme_minimal()
 
 # Create the heatmap of percentage of days in each year exceeding the WHO health-based guideline, including all 4 monitoring sites
 ggplot(pm25_exceedance,
@@ -72,12 +58,9 @@ ggplot(pm25_exceedance,
     name = "% of days > 5 µg/m³",
     low = "white",
     high = "red",
-    limits = c(0, 100))
-  +
+    limits = c(0, 100)) +
   labs(title = "Percentage of days exceeding WHO PM2.5 guideline",
     x = "Year",
-    y = "Monitoring site")
-   +
-  theme_minimal()
+    y = "Monitoring site") +  theme_minimal()
 
 
